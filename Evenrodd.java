d5adba4b519ee4f58637aa3a6365b4eda5650d55
1901kushu <131339696+1901kushu@users.noreply.github.com>
2023-06-02 17:06:36 +0530

    Add files via upload

---
+++ Evenrodd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class evenorodd{
+public static boolean is even(int n){
+return(n/2*2==n);
+}
+public static void main(String args[])
+{
+Scanner scob=new Scanner(System.in);
+System.out.println("Enter a number");
+int n=scob.nextInt();
+if(isEven(n){
+System.out.println("Even\n");
+}
+else
+{
+System.out.println("Odd\n");
+}
+}}
---
+++ InputMonthNumberIf.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class InputMonthNumberIf{
+public static void main(String args[])
+{
+Scanner scob=new Scanner(System.in);
+int Mn;
+Mn=scob.nextInt();
+if(Mn==1){System.out.println("JANUARY");}
+else if(Mn==2){System.out.println("FEBRUARY");}
+else if(Mn==3){System.out.println("MARCH");}
+else if(Mn==4){System.out.println("APRIL");}
+else if(Mn==5){System.out.println("MAY");}
+else if(Mn==6){System.out.println("JUNE");}
+else if(Mn==7){System.out.println("JULY");}
+else if(Mn==8){System.out.println("AUGUST");}
+else if(Mn==9){System.out.println("SEPTEMBER");}
+else if(Mn==10){System.out.println("OCTOBER");}
+else if(Mn==11){System.out.println("NOVEMBER");}
+else if(Mn==12){System.out.println("DECEMBER");}
+else {System.out.println("INVALID");}
+}}
