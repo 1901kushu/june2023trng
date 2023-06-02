cd7525c5e59804bdfb220cb9ebd9bb66aabf3e05
1901kushu <131339696+1901kushu@users.noreply.github.com>
2023-06-02 17:05:16 +0530

    Add files via upload

---
+++ MSD.java
@@ -0,0 +1,16 @@
+class Csk{
+public void funA1(){
+System.out.println("Chennai Super Kings");
+}
+public void funA2(){
+System.out.println("Five Time Champions");
+}
+}
+class Dhoni
+{
+public static void main(String args[]){
+Csk ob=new Csk();
+ob.funA1();
+ob.funA2();
+}
+}
